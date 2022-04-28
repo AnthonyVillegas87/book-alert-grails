a6df163d34374422e19a565f922df3b849b905e3
Anthony Villegas <villegasanthony87@yahoo.com>
2022-04-28 15:08:19 -0500

    feat: established Book domain setup

--- grails-app/domain/book/alert/grails/Book.groovy
+++ grails-app/domain/book/alert/grails/Book.groovy
@@ -1,7 +1,121 @@
 package book.alert.grails
 
 class Book {
+    Integer id
+    String title
+    String description
+    Date releaseDate
+    String itunesUrl
+    String artworkUrl
+    Author author
+    long externalId
+    List<BookUser> bookUser
+
+
+    static belongsTo = Author
+    static hasMany = [author: Author, bookUser: BookUser]
+
 
     static constraints = {
+        title(nullable: false, maxSize: 100)
+        description(nullable: false, maxSize: 10000)
+        releaseDate format: 'yyyy-MM-dd'
+        itunesUrl(url: true)
+        artworkUrl(url: true)
+
+    }
+
+
+
+    Book(Integer id, String title, String description, Date releaseDate, String itunesUrl, String artworkUrl, Author author, long externalId, List<BookUser> bookUser) {
+        this.id = id
+        this.title = title
+        this.description = description
+        this.releaseDate = releaseDate
+        this.itunesUrl = itunesUrl
+        this.artworkUrl = artworkUrl
+        this.author = author
+        this.externalId = externalId
+        this.bookUser = bookUser
+    }
+
+    Integer getId() {
+        return id
+    }
+
+    void setId(Integer id) {
+        this.id = id
+    }
+
+    String getTitle() {
+        return title
+    }
+
+    void setTitle(String title) {
+        this.title = title
+    }
+
+    String getDescription() {
+        return description
+    }
+
+    void setDescription(String description) {
+        this.description = description
+    }
+
+    Date getReleaseDate() {
+        return releaseDate
+    }
+
+    void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate
+    }
+
+    String getItunesUrl() {
+        return itunesUrl
+    }
+
+    void setItunesUrl(String itunesUrl) {
+        this.itunesUrl = itunesUrl
+    }
+
+    String getArtworkUrl() {
+        return artworkUrl
+    }
+
+    void setArtworkUrl(String artworkUrl) {
+        this.artworkUrl = artworkUrl
+    }
+
+    Author getAuthor() {
+        return author
+    }
+
+    void setAuthor(Author author) {
+        this.author = author
+    }
+
+    long getExternalId() {
+        return externalId
+    }
+
+    void setExternalId(long externalId) {
+        this.externalId = externalId
+    }
+
+    List<BookUser> getBookUser() {
+        return bookUser
+    }
+
+    void setBookUser(List<BookUser> bookUser) {
+        this.bookUser = bookUser
+    }
+
+    static getConstraints() {
+        return constraints
+    }
+
+    static void setConstraints(constraints) {
+        Book.constraints = constraints
     }
 }
---
+++ grails-app/domain/book/alert/grails/BookUser.groovy
@@ -0,0 +1,4 @@
+package book.alert.grails
+
+class BookUser {
+}
